72b91af793080212ece4a0efe9a2c5d1e2d1c424
Neal <victor.negocios.l.17@gmail.com>
2023-10-09 00:58:45 -0500

    creacion del componente card

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,60 @@ function App() {
 
   // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
   useEffect(() => {
-     getData();
+    getData();
   }, []);
 
   // Puedes ver la variable data en consola.
   console.log(data);
   return (
     <>
-    {/* Aquí te dejo un ejemplo de cómo podrías imprimir varios elementos a la vez. */}
-      {data.map((el, i) => {
-        return <h1 key={i}>{el.city}</h1>;
-      })}
+      <main>
+        <div className="container ">
+          <div className=" my-3 row d-flex flex-row">
+            {/* Aquí te dejo un ejemplo de cómo podrías imprimir varios elementos a la vez. */}
+            {data.map((el, i) => {
+              return (
+                <div className="col-4" key={i}>
+                  <div className="card my-3">
+                    <img
+                      src={el.photo}
+                      className="card-img-top "
+                      alt=""
+                      style={{ borderRadius: "10px" }}
+                    />
+                    <div className="card-body col-12">
+                      <div className="d-flex justify-content-between heigh ">
+                        <div className="d-flex gap-1 align-items-center">
+                          {el.superHost ? (
+                            <p className="superHost">SUPER HOST</p>
+                          ) : (
+                            ""
+                          )}
+                          <p className="par2">
+                            {el.type + ". " + el.beds + " beds"}
+                          </p>
+                        </div>
+                        <div className="d-flex gap-2 align-items-center">
+                          <div>
+                            <i className="fa-solid fa-star"></i>
+                          </div>
+                          <div>
+                            <p className="raiting">{el.rating}</p>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="my-1">
+                        <p className="title">{el.title}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
